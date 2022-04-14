c57a4c0390a933b0ed314b14c71d3627baecb4c8
João Meirinho <meirinhojoao2004@gmail.com>
2022-04-14 14:56:58 -0300

    add file 'int.php'

---
+++ sumary/tipos/int.php
@@ -0,0 +1,21 @@
+<div class="titulo">Tipo inteiro</div>
+
+<?php
+
+echo 11;
+echo '<br>';
+
+var_dump(11);
+
+echo '<br>';
+
+echo PHP_INT_MAX, '<br>';
+
+echo PHP_INT_MIN, '<br>';
+
+echo 011, '<br>'; //base octal
+
+echo 0b11001100110, '<br>'; //base binária
+
+echo 0x11, '<br>'; //base hexadecimal
+
